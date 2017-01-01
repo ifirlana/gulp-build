f5354854e070f69811d47a251f951edd76dfa3a3
ikhlas firlana <ifirlana@gmail.com>
2017-01-01 18:21:11 +0700

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,32 @@
+"use strict";
+
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var myth = require("gulp-myth");
 var uglify = require("gulp-uglify");
+var jshint = require("gulp-jshint");
+var imagemin = require("gulp-imagemin");
 
 // styles
 gulp.task("styles", function () {
-    return gulp.src("css/*.css")
+    return gulp.src("app/css/*.css")
         .pipe(concat("all.css"))
+        .pipe(myth())
         .pipe(gulp.dest("dist/"));
 });
 
+// image
+gulp.task("images", function () {
+    return gulp.src("app/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+})
+
 // scripts
 gulp.task("scripts", function () {
-    return gulp.src("js/*.js")
+    return gulp.src("app/js/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"))
         .pipe(concat("all.js"))
         .pipe(uglify())
         .pipe(gulp.dest("dist/"));
@@ -19,8 +34,9 @@ gulp.task("scripts", function () {
 
 // watch
 gulp.task("watch", function () {
-    gulp.watch("css/*.css", "styles");
-    gulp.watch("js/*.js", "scripts");
+    gulp.watch("app/css/*.css", "styles");
+    gulp.watch("app/js/*.js", "scripts");
+    gulp.watch("app/img/*","images")
 });
 
 // default
